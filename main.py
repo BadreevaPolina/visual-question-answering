34d91807fa2e3a6f89571de30562d29883003d1a
Polina Badreeva <badreeva.2003@gmail.com>
2022-12-15 20:06:54 +0300

    Change main and add README

--- main.py
+++ main.py
@@ -2,30 +2,32 @@ import streamlit as st
 from transformers import ViltProcessor, ViltForQuestionAnswering
 from PIL import Image
 
-processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
+def forward_pass(image, text):
+    processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+    model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
-def forward_pass():
     encoding = processor(image, text, return_tensors="pt")
     outputs = model(**encoding)
     logits = outputs.logits
-    return logits.argmax(-1).item()
+    idx = logits.argmax(-1).item()
+    st.write("Predicted answer:", model.config.id2label[idx])
 
 
-def show_image():
+def save_image(bottom_image):
     image = Image.open(bottom_image)
     new_image = image.resize((704, 480))
     st.image(new_image)
+    return new_image
 
 
-if __name__ == "__main__":
+def main():
     st.title("Visual Question Answering")
     image = None
 
     bottom_image = st.file_uploader('', type='jpg', key=6)
     if bottom_image is not None:
-        show_image()
+        image = save_image(bottom_image)
 
     with st.form(key="question"):
         text = st.text_input(label="Answer your question:")
@@ -33,9 +35,12 @@ if __name__ == "__main__":
         if submit:
             if text is not None:
                 if image is not None:
-                    idx = forward_pass()
-                    st.write("Predicted answer:", model.config.id2label[idx])
+                    forward_pass(image, text)
                 else:
                     st.write("No image")
             else:
                 st.write("No text")
+
+
+if __name__ == "__main__":
+    main()
